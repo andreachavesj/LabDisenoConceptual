7411459f1c1f97be99e39af3d73f143678240ad4
Andrea <Andrea@RAZOR>
2023-11-02 20:26:30 -0600

    First commit

---
+++ LaboratorioEnClase/BookStoreApi/Modelos/Alumno.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace BookStoreApi.Modelos;
+
+public class Alumno
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? _id { get; set; }
+
+
+    [BsonElement("cedula")]
+    [JsonPropertyName("cedula")]
+    public string cedula { get; set; } = null!;
+
+    public decimal nombre { get; set; }
+
+    public string telefono { get; set; } = null!;
+
+    public string email { get; set; } = null!;
+    
+    public string fecha_nacimiento { get; set; } = null!;
+    
+    public string carrera { get; set; } = null!;
+
+
+}
---
+++ LaboratorioEnClase/BookStoreApi/Modelos/ProyectoDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStoreApi.Modelos
+{
+    public class ProyectoDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string AlumnoCollectionName { get; set; } = null!;
+    }
+}
