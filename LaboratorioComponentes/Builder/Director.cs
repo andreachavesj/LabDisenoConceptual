03605d8506da703b90fe6ada7b3251a352b44910
MCQuire08 <rjquiros21@gmail.com>
2023-11-09 20:34:51 -0600

    Creacion del Patron Builder

---
+++ LaboratorioComponentes/Builder/Director.cs
@@ -0,0 +1,27 @@
+﻿using LaboratorioComponentes.IBuilder;
+using LaboratorioComponentes.Models;
+
+namespace LaboratorioComponentes.Builder
+{
+    public class Director
+    {
+        private IBuilderAlumno alumnoBuilder;
+
+        public Director(IBuilderAlumno alumnoBuilder) 
+        {
+            this.alumnoBuilder = alumnoBuilder;
+        }
+
+        public Alumno CrearAlumno(string cedula, string nombre, string telefono, string email, string fechaNacimiento, string carrera)
+        {
+            return alumnoBuilder
+                .SetCarrera(carrera)
+                .SetEmail(email)
+                .SetCedula(cedula)
+                .SetTelefono(telefono)
+                .SetFechaNacimiento(fechaNacimiento)
+                .SetNombre(nombre)
+                .Build();
+        }
+    }
+}
---
+++ LaboratorioComponentes/IBuilder/IBuilderAlumno.cs
@@ -0,0 +1,15 @@
+﻿using LaboratorioComponentes.Models;
+
+namespace LaboratorioComponentes.Builder
+{
+    public interface IBuilderAlumno
+    {
+        IBuilderAlumno SetNombre(String nombre);
+        IBuilderAlumno SetCedula(String cedula);
+        IBuilderAlumno SetTelefono(String telefono);
+        IBuilderAlumno SetEmail(String email);
+        IBuilderAlumno SetFechaNacimiento(String fechaNacimiento);
+        IBuilderAlumno SetCarrera(String carrera);
+        Alumno Build();
+    }
+}
